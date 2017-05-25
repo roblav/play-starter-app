aa877605894959c7b33c6e9c3bb77b3dc4282b9f
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-05-25 16:27:03 +0100

    Step 3: Create a basic form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,32 @@
 package controllers
 
+import javax.inject.Inject
+
+import models.Widget
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
 
-class Application extends Controller {
+class Application @Inject()(val messagesApi:MessagesApi) extends Controller with I18nSupport {
 
-  def listWidgets = Action {
+  def listWidgets = Action { implicit request =>
     // Pass an unpopulated form to the template
-    Ok(views.html.listWidgets())
+    Ok(views.html.listWidgets(Application.createWidgetForm))
   }
+
+  def createWidget = TODO
+
+}
+
+object Application {
+
+  val createWidgetForm = Form(
+    mapping(
+      "name" -> text,
+      "price" -> number
+    )(Widget.apply)(Widget.unapply)
+  )
 
 }
